813ec1c1a4977ccc76de45d8df402f656c521672
Lily-Sneha <lakralily3@gmail.com>
2023-07-09 10:45:40 +0530

    changed media publication design

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const marqueeContent = document.querySelector("ul.marquee-content");
 
 root.style.setProperty("--marquee-elements", marqueeContent.children.length);
 
-for(let i=0; i<marqueeElementsDisplayed; i++) {
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
   marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
 }
 /*!-----------------------------------------------------------------------------
@@ -19,121 +19,122 @@ for(let i=0; i<marqueeElementsDisplayed; i++) {
 
 function easy_background(selector, sld_args) {
 
-    function empty_img(x) {
-      if (x) {
-        return "<img src='" + x + "'>";
-      } else {
-        return "";
+  function empty_img(x) {
+    if (x) {
+      return "<img src='" + x + "'>";
+    } else {
+      return "";
+    }
+  }
+
+  //if delay is empty or forgotten then use this default value
+  var def_del = 3000;
+
+  var p = document.createElement("div");
+  p.innerHTML = " ";
+  p.classList.add("easy_slider");
+
+  document.body.insertBefore(p, document.body.firstChild);
+  //switch all values in object -- objectname.index in you case sld_args is object and i is index of array which keep images (i). We use this function for fill div with img tags
+  //and for insert delays into empty or forgotten places in object
+  sld_args.slide.forEach(function (v, i) {
+    if (v) {
+      document.querySelector(".easy_slider").innerHTML += empty_img(v);
+      if (typeof sld_args.delay[i] == 'undefined' || typeof sld_args.delay[i] == '' || sld_args.delay[i] == 0) {
+        sld_args.delay[i] = def_del;
       }
     }
 
-    //if delay is empty or forgotten then use this default value
-    var def_del = 3000;
-  
-    var p = document.createElement("div");
-    p.innerHTML = " ";
-    p.classList.add("easy_slider");
-  
-    document.body.insertBefore(p, document.body.firstChild);
-    //switch all values in object -- objectname.index in you case sld_args is object and i is index of array which keep images (i). We use this function for fill div with img tags
-    //and for insert delays into empty or forgotten places in object
-    sld_args.slide.forEach(function(v, i) {
-      if (v) {
-        document.querySelector(".easy_slider").innerHTML += empty_img(v);
-        if (typeof sld_args.delay[i] == 'undefined' || typeof sld_args.delay[i] == '' || sld_args.delay[i] == 0) {
-          sld_args.delay[i] = def_del;
-        }
-      }
-  
-    });
-  
+  });
+
+  //add various style on selector
+  document.querySelector(".easy_slider").style.display = "none";
+
+
+  //add various style on selector
+  document.querySelector(selector).style.backgroundSize = "cover";
+  document.querySelector(selector).style.backgroundRepeat = "no-repeat";
+  document.querySelector(selector).style.backgroundPosition = "center center";
+
+
+  setTimeout(function () {
     //add various style on selector
-    document.querySelector(".easy_slider").style.display = "none";
-  
-  
-    //add various style on selector
-    document.querySelector(selector).style.backgroundSize = "cover";
-    document.querySelector(selector).style.backgroundRepeat = "no-repeat";
-    document.querySelector(selector).style.backgroundPosition = "center center";
-  
-  
-    setTimeout(function() {
-      //add various style on selector
-      if (typeof sld_args.transition_timing === 'undefined') {
-        sld_args.transition_timing = "ease-in";
-      }
-      if (typeof sld_args.transition_duration === 'undefined') {
-        sld_args.transition_duration = 500;
-      }
-      var transition = "all " + sld_args.transition_duration + 'ms ' + sld_args.transition_timing;
-      document.querySelector(selector).style.WebkitTransition = transition;
-      document.querySelector(selector).style.MozTransition = transition;
-      document.querySelector(selector).style.MsTransition = transition;
-      document.querySelector(selector).style.OTransition = transition;
-      document.querySelector(selector).style.transition = transition;
-    }, 100);
-  
-  
-    //this n is number of row  in object - if first row one function if more than 1 then other
-    var n = 1;
-    //li collection previous delays from previous slides
-    var li = 0;
-  
-    function slider() {
-      //switching all images one by one
-      sld_args.slide.forEach(function(vvv, iii) {
-        //here go all slides except first
-        if (n > 1) {
-          //set delay from collected number from previous slides
-          var delay = li;
-          setTimeout(function() {
-  
-            document.querySelector(selector).style.backgroundImage = "url('" + vvv + "')";
-  
-          }, delay); // >1
-          //collecting delays from curent
-          li = li + sld_args.delay[iii];
-  
-        } else { //this function for only  first slide
-  
-          //next row
-          n++;
-          //collect delay first time
-          li = sld_args.delay[iii];
-  
+    if (typeof sld_args.transition_timing === 'undefined') {
+      sld_args.transition_timing = "ease-in";
+    }
+    if (typeof sld_args.transition_duration === 'undefined') {
+      sld_args.transition_duration = 500;
+    }
+    var transition = "all " + sld_args.transition_duration + 'ms ' + sld_args.transition_timing;
+    document.querySelector(selector).style.WebkitTransition = transition;
+    document.querySelector(selector).style.MozTransition = transition;
+    document.querySelector(selector).style.MsTransition = transition;
+    document.querySelector(selector).style.OTransition = transition;
+    document.querySelector(selector).style.transition = transition;
+  }, 100);
+
+
+  //this n is number of row  in object - if first row one function if more than 1 then other
+  var n = 1;
+  //li collection previous delays from previous slides
+  var li = 0;
+
+  function slider() {
+    //switching all images one by one
+    sld_args.slide.forEach(function (vvv, iii) {
+      //here go all slides except first
+      if (n > 1) {
+        //set delay from collected number from previous slides
+        var delay = li;
+        setTimeout(function () {
+
           document.querySelector(selector).style.backgroundImage = "url('" + vvv + "')";
-  
-        }
-  
-      });
-  
-    };
-  
+
+        }, delay); // >1
+        //collecting delays from curent
+        li = li + sld_args.delay[iii];
+
+      } else { //this function for only  first slide
+
+        //next row
+        n++;
+        //collect delay first time
+        li = sld_args.delay[iii];
+
+        document.querySelector(selector).style.backgroundImage = "url('" + vvv + "')";
+
+      }
+
+    });
+
+  };
+
+  slider();
+
+  setInterval(function () { // REPEAT
+
     slider();
-  
-    setInterval(function() { // REPEAT
-  
-      slider();
-      //here used length of array of delays in object instead you tot_time variable
-    }, sld_args.delay.length);
-  
-  }
-  
-  easy_background(".hero",
-        
+    //here used length of array of delays in object instead you tot_time variable
+  }, sld_args.delay.length);
+
+}
+
+easy_background(".hero",
+
   {
-    slide: ["public/img/Partners-Img/Azim_Premji_University_logo..webp", 
-    "public/img/Partners-Img/Azim_Premji_University_logo..webp", 
-    "public/img/Partners-Img/Azim_Premji_University_logo..webp", 
-    "public/img/Partners-Img/Azim_Premji_University_logo..webp"],
-  
+    slide: ["public/img/Partners-Img/Azim_Premji_University_logo..webp",
+      "public/img/Partners-Img/Azim_Premji_University_logo..webp",
+      "public/img/Partners-Img/Azim_Premji_University_logo..webp",
+      "public/img/Partners-Img/Azim_Premji_University_logo..webp"],
+
     delay: [3000, 3000, 3000, 20000]
   }
-  
-  );
+
+);
 
 
 //Javascript to toggle the menu
-document.getElementById('nav-toggle').onclick = function(){
-    document.getElementById("nav-content").classList.toggle("hidden");
+document.getElementById('nav-toggle').onclick = function () {
+  document.getElementById("nav-content").classList.toggle("hidden");
 }
+
